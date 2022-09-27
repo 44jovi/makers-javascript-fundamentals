246c98441ea82a4bfa64fb3e9d892645be72114b
Jovi <kwong.jovi@gmail.com>
2022-09-27 15:31:29 +0100

    refactor(bite12 challenge1)

--- bite12/challenge1/ShoppingBasket.js
+++ bite12/challenge1/ShoppingBasket.js
@@ -1,22 +1,27 @@
 class ShoppingBasket {
 
-constructor() {
-  this.basket = [];
-};
+  constructor() {
+    this.basket = [];
+  };
 
-addItem(candy) {
-  this.basket.push(candy);
-};
+  addItem(candy) {
+    this.basket.push(candy);
+  };
 
-getTotalPrice() {
-  const pricesOnly = this.basket.map(candy =>
-    candy.price
-  );
-  return priceOnly.reduce((previousValue, currentValue) =>  previousValue + currentValue, 0);
-};
+  getTotalPrice() {
+    return this.basket.reduce((previousValue, candy) =>  previousValue + candy.price, 0);
+  };
 }
 
 module.exports = ShoppingBasket;
 
 // notes:
 // [1,2,3].reduce((prevVal, currVal) => prevVal + currVal, 0);
+
+// original code using map
+// getTotalPrice() {
+//   const pricesOnly = this.basket.map(candy =>
+//     candy.price
+//   );
+//   return priceOnly.reduce((previousValue, currentValue) =>  previousValue + currentValue, 0);
+// };
